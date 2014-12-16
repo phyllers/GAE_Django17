8901bb00d6541d04bdf85c7fa39f10ae9361e5ef
Kelly Iverson <kiverson@systemsbiology.org>
2014-12-16 14:20:37 -0800

    took out unused file main.js

--- static/js/main.js
+++
@@ -1,7 +0,0 @@
-function check_user(is_authenticated){
-    console.log('in check user and user authenticated is ' + is_authenticated);
-    if (!is_authenticated){
-        console.log('signing out');
-        google.identitytoolkit.signOut();
-    }
-}
