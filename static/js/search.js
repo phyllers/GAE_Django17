a0672757d433e912c86af7369885d7beafc2d454
phyllers <plee@systemsbiology.org>
2014-12-16 15:29:28 -0800

    updated search results page, added sections to search criteria

--- static/js/search.js
+++ static/js/search.js
@@ -1,35 +1,63 @@
 $(document).ready(function() {
     var elements_selected = {};
     var platforms_selected = {};
+    var tumors_selected = {};
 
-    var add_element = function(id, text) {
-        var item = $('<li class="' + id + '">'+text+'</li>');
-        $('#search-criteria').append(item);
-    }
-    var remove_element = function(id) {
-        $('#search-criteria .' + id).remove();
-    }
-
+    var update_search = function(section, items) {
+        $('#'+ section +'-search-criteria').empty();
+        if (!$.isEmptyObject(items)) {
+            $('#'+ section +'-search-criteria').append('<h5>'+section+' selected:</h5');
+            $('#'+ section +'-search-criteria').append('<ul></ul>');
+            for (var e in items) {
+                var item = $('<li class="' + e + '">'+items[e]+'</li>');
+                $('#'+ section +'-search-criteria ul').append(item);
+            }
+        }
+    };
 
     $('[name="elements-selected"]').on('change', function(){
         if (this.checked) {
             elements_selected[this.id] = this.labels[0].innerText.replace(/^\s+|\s+$/g, '');
-            add_element(this.id, elements_selected[this.id]);
         } else {
             delete elements_selected[this.id];
-            remove_element(this.id);
         }
-        console.log(elements_selected);
-    })
+        update_search('elements', elements_selected);
+    });
 
     $('[name="platforms-selected"]').on('change', function(){
         if (this.checked) {
-            elements_selected[this.id] = this.labels[0].innerText.replace(/^\s+|\s+$/g, '');
-            add_element(this.id, elements_selected[this.id]);
+            platforms_selected[this.id] = this.labels[0].innerText.replace(/^\s+|\s+$/g, '');
         } else {
-            delete elements_selected[this.id];
-            remove_element(this.id);
+            delete platforms_selected[this.id];
         }
-        console.log(elements_selected);
+        update_search('platforms', platforms_selected);
+    });
+
+    $('[name="tumors-selected"]').on('change', function(){
+        if (this.checked) {
+            tumors_selected[this.id] = this.labels[0].innerText.replace(/^\s+|\s+$/g, '');
+            console.log(tumors_selected);
+        } else {
+            delete tumors_selected[this.id];
+        }
+        update_search('tumors', tumors_selected);
+    });
+
+    $('#select-all').on('click', function() {
+        if (this.checked) {
+            $('table input[type="checkbox"]').each(function() {
+                this.checked="checked";
+            })
+        } else {
+            $('table input[type="checkbox"]').each(function() {
+                this.checked="";
+            })
+        }
+    });
+
+    $('table input[type="checkbox"]').on('click', function() {
+        if (!this.checked) {
+            $('#select-all')[0].checked="";
+        }
     })
 });
