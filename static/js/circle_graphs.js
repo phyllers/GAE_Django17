61b538186f573255e756531f24b189a67ffbf26c
Kelly Iverson <kiverson@systemsbiology.org>
2015-01-13 16:12:56 -0800

    modified gitignore

---
+++ static/js/circle_graphs.js
@@ -0,0 +1,9 @@
+d3.tsv("../clinMut_10054_56_02.tsv")
+    .header("Content-Type", "application/json")
+    .get(function(error, data){
+      circleg.mutation_array_data = data;        
+      draw_circle_graphs();
+    });
+
+
+ var circleg = circleg || {};
